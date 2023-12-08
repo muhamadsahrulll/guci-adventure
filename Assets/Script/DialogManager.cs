2033cb30940880e531f8978a5d1fcc4a478c0b22
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2023-12-08 10:16:01 +0700

    update map 1

--- Assets/Script/DialogManager.cs
+++ Assets/Script/DialogManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI namaText;
+    public TextMeshProUGUI dialogText;
+    public Animator animator;
+
     private Queue<string> sentences;
 
     private void Start()
@@ -13,6 +20,46 @@ public class DialogManager : MonoBehaviour
 
     public void StartDialog(Dialog dialog)
     {
-        Debug.Log("Start dialog" + dialog.name);
+        animator.SetBool("isOpen", true);
+
+        namaText.text = dialog.name;
+
+        sentences.Clear();
+        foreach ( string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
     }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+        
+    }
+
+    void EndDialog()
+    {
+        animator.SetBool("isOpen", false);
+    }
+
+
 }
