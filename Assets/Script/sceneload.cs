14a94bb9b2fff6d4470d0b6afc6fd545259f0a93
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-01-14 19:59:02 +0700

    fix

--- Assets/Script/sceneload.cs
+++ Assets/Script/sceneload.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class sceneload : MonoBehaviour
 {
+    public PlayerData playerData;
     public void loadScene (string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void keluar()
+    {
+        Application.Quit();
+    }
+
+    public void add100Coins()
+    {
+        playerData.coins += 100;
     }
 }
