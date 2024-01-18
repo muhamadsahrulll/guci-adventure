21ac974e07461c7783f31ef51784a7bd68cbf261
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-01-18 17:03:45 +0700

    fix bug and kuda

--- Assets/Script/CampBuilder.cs
+++ Assets/Script/CampBuilder.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class CampBuilder : MonoBehaviour
 {
-    public GameObject campPrefab; // Seret prefab tenda ke sini
+    
     public PlayerData playerData; // Referensi ke PlayerData script
     public MissionManager missionManager; // Referensi ke MissionManager script
     public int buildCost = 100; // Biaya membangun tenda
     public GameObject tidakCukup;
     public GameObject sudahTenda;
     public GameObject belumTenda;
+    public GameObject tenda;
+    public GameObject tendaBtn;
+    public GameObject Tendabangun;
 
-    private void Start()
+    public void Start()
     {
         // Pastikan objek tenda tidak aktif saat awal game
         //campPrefab.SetActive(false);
-        missionManager.campbuilder = FindObjectOfType<CampBuilder>();
+        //missionManager.campbuilder = FindObjectOfType<CampBuilder>();
         missionManager.LoadMissionStatus();
-
+        //tenda.SetActive(false);
         
     }
+    
 
     public void BuildCamp()
     {
+        if (missionManager.IsCampBuilt())
+        {
+            Debug.Log("Tenda sudah dibangun!");
+            StartCoroutine(sudahTendabangun(2f));
+            return; // Keluar dari fungsi jika tenda sudah dibangun
+        }
+
         // Periksa apakah pemain memiliki cukup koin untuk membangun tenda
         if (playerData.coins >= buildCost)
         {
@@ -31,7 +42,7 @@ public class CampBuilder : MonoBehaviour
             playerData.AddCoins(-buildCost);
 
             // Aktifkan objek tenda di posisi pemain
-            campPrefab.gameObject.SetActive(true);
+            tenda.gameObject.SetActive(true);
             missionManager.SetCampBuilt(); // Panggil metode untuk memberi tahu MissionManager bahwa tenda telah dibangun
             sudahTenda.gameObject.SetActive(true);
             belumTenda.gameObject.SetActive(false);
@@ -49,5 +60,12 @@ public class CampBuilder : MonoBehaviour
         tidakCukup.gameObject.SetActive(true);
         yield return new WaitForSeconds(seconds);
         tidakCukup.gameObject.SetActive(false);
+    }
+
+    IEnumerator sudahTendabangun(float seconds)
+    {
+        Tendabangun.gameObject.SetActive(true);
+        yield return new WaitForSeconds(seconds);
+        Tendabangun.gameObject.SetActive(false);
     }
 }
--- Assets/Script/MissionManager.cs
+++ Assets/Script/MissionManager.cs
@@ -104,7 +104,8 @@ public class MissionManager : ScriptableObject
             if (isCampBuilt)
             {
                 Debug.Log("Load status tenda: Tenda sudah dibangun!");
-                campbuilder.campPrefab.SetActive(true);
+                campbuilder.tendaBtn.gameObject.SetActive(false);
+                campbuilder.tenda.gameObject.SetActive(true);
                 campbuilder.sudahTenda.SetActive(true);
                 // Tambahkan logika atau pembaruan yang diperlukan ketika tenda sudah dibangun
 
