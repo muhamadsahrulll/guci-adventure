8587dba2e3080d99dba4dec77272bf8887b235ba
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2024-01-22 19:15:14 +0700

    add loading screen done

---
+++ Assets/Script/loadingscreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingscreen : MonoBehaviour
+{
+    public GameObject loading1;
+
+    DialogTrigger dialogTrigger;
+
+    private void Start()
+    {
+        //loading1 = dialogTrigger.TriggerDialog();
+    }
+}
